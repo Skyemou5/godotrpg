a6c39a9081712430e9e5d0fa499f25b883c3acf5
ben <benjaminshurts@gmail.com>
2020-09-03 11:40:37 -0600

    move and attack

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,14 @@ const FRICTION = 500
 const MAX_SPEED = 80
 const ACCELERATION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 var speed: float = 4
 var velocity = Vector2.ZERO
 
@@ -12,7 +20,22 @@ onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			MoveState(delta)
+		ROLL:
+			pass
+		ATTACK:
+			AttackState(delta)
+
+func AttackState(delta):
+	animationState.travel("Attack")
+
+func MoveState(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -22,13 +45,17 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector) #cant change direction mid attack
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-	print(velocity)
-	
+
 	velocity = move_and_slide(velocity)
- 
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func AttackAnimFinished():
+	state = MOVE
